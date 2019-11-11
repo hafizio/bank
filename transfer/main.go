770fdf95c66267959a58c1cad167802c3d28d576
jojokys <54433564+jojokys@users.noreply.github.com>
2019-11-11 12:50:36 +0800

    Update main.go

--- transfer/main.go
+++ transfer/main.go
@@ -14,19 +14,30 @@ type Transfer struct {
 	//not sure if bankaccount needed to include in the struct
 	//Bankaccount_transferor string `json:"bankaccount_transferor"`
 	//Bankaccount_transferee string `json:"bankaccount_transferee"`
+	Message string `json:"message"`
 	Balance_transferor float64 `json:"balance_transferor"`
 	Balance_transferee float64 `json:"balance_transferee"`
+	
+}
+
+func status(balance_transferor, balance_transferee, transfer_amount float64) string {
+	var balance_remaining float64
+	var status string
+	balance_remaining = balance_transferor - transfer_amount
+	if balance_remaining < 0 {
+		status = fmt.Sprintf("Balance is insufficient. Request aborted. Balance is short in amount of MYR %0.2f ", math.Abs(balance_remaining))
+	} else {
+		status = fmt.Sprintf("Transfer succeeds: Balance remain MYR %0.2f", balance_remaining)
+	}
+	return status
 }
 
 func result(balance_transferor, balance_transferee, transfer_amount float64) (float64, float64) {
-	fmt.Println("Transferring...")
 	var balance_remaining float64
 	balance_remaining = balance_transferor - transfer_amount
 
 	if balance_remaining < 0 {
-		fmt.Printf("Balance is insufficient. Request aborted.\nBalance is short in amount of MYR %0.2f\n", math.Abs(balance_remaining))
 	} else {
-		fmt.Printf("Transfer succeeds: Balance remain MYR %0.2f\n", balance_remaining)
 		balance_transferee += transfer_amount
 		balance_transferor -= transfer_amount
 	}
@@ -43,9 +54,11 @@ func main() {
 		balance_transferor, _ := strconv.ParseFloat(c.FormValue("balance_transferor"), 64)
 		balance_transferee, _ := strconv.ParseFloat(c.FormValue("balance_transferee"), 64)
 		transfer_amount, _ := strconv.ParseFloat(c.FormValue("transfer_amount"), 64)
+		ans := status(balance_transferor, balance_transferee, transfer_amount)
 		balance_transferor, balance_transferee = result(balance_transferor, balance_transferee, transfer_amount)
 
 			transfer := &Transfer{
+				Message: ans,
 				Balance_transferor: balance_transferor,
 				Balance_transferee: balance_transferee,
 			}
@@ -55,4 +68,3 @@ func main() {
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
-
